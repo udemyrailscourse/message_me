0e7b5528f5ae9a675104750a96a7c337a9f90904
Mashrur Hossain <mashrur.hossain@gmail.com>
2018-12-03 22:42:28 +0100

    Restrict views from controller layer

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,4 +1,5 @@
 class ChatroomController < ApplicationController
+  before_action :require_user
 
   def index
     @messages = Message.all
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  before_action :logged_in_redirect, only: [:new, :create]
 
   def new
   end
@@ -19,6 +20,15 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     flash[:success] = "You have successfully logged out"
     redirect_to login_path
+  end
+
+  private
+
+  def logged_in_redirect
+    if logged_in?
+      flash[:error] = "You are already logged in"
+      redirect_to root_path
+    end
   end
 
 end
