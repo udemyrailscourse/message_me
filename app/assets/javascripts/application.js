37e21b221afdcf8a0087ca1f2299e23f1224edef
Mashrur Hossain <mashrur.hossain@gmail.com>
2018-12-03 17:44:16 +0100

    Enable flash messages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,4 +19,7 @@
 
 $(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
+  $('.message .close').on('click', function() {
+    $(this).closest('.message').transition('fade');
+  });
 })
