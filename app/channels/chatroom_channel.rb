5d1e781c93b4960ee361dbed5eb60924e3bb8dc7
Mashrur Hossain <mashrur.hossain@gmail.com>
2018-12-30 14:32:06 +0100

    Generate chatroom channel and add route

---
+++ app/channels/chatroom_channel.rb
@@ -0,0 +1,9 @@
+class ChatroomChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "chatroom_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   post 'message', to: 'messages#create'
+
+  mount ActionCable.server, at: '/cable'
 end
