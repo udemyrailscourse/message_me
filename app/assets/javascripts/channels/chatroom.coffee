2dfab834e25ab5c51276a026f2068dbe382cf411
Mashrur Hossain <mashrur.hossain@gmail.com>
2019-01-02 15:45:01 +0100

    Display messages realtime

--- app/assets/javascripts/channels/chatroom.coffee
+++ app/assets/javascripts/channels/chatroom.coffee
@@ -6,4 +6,4 @@ App.chatroom = App.cable.subscriptions.create "ChatroomChannel",
     # Called when the subscription has been terminated by the server
 
   received: (data) ->
-    alert data.foo
+    $('#message-container').append data.mod_message
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,7 @@ class MessagesController < ApplicationController
     message = current_user.messages.build(message_params)
     if message.save
       ActionCable.server.broadcast "chatroom_channel",
-                                    foo: message.body
+                                    mod_message: message_render(message)
     end
   end
 
@@ -13,6 +13,10 @@ class MessagesController < ApplicationController
 
   def message_params
     params.require(:message).permit(:body)
+  end
+
+  def message_render(message)
+    render(partial: 'message', locals: {message: message})
   end
 
 end
